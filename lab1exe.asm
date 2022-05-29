5ba409d4630414a4f0f77c6da928d48f510fc228
Lipki3 <73040453+Lipki3@users.noreply.github.com>
2022-05-29 12:16:32 +0300

    lab1

---
+++ lab1exe.asm
@@ -0,0 +1,13 @@
+.model small
+.stack 100h
+.code
+start: mov ax,@data
+mov ds,ax
+mov dx,offset message
+mov ah,9
+int 21h
+mov ax,4C00h
+int 21h
+.data
+message db "Hello World!",0Dh,0Ah,'$'
+end start
